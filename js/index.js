1b4128aada464de0ad0f33bcdfd6d9d3bf22af37
Vijay Koladara <abc.vijaykoladara212@gmail.com>
2022-01-05 12:42:59 +0530

    Complete Work Done

--- js/index.js
+++ js/index.js
@@ -1,25 +1,17 @@
 let counter = 1;
 
 function load() {
-  $("#info").show();
+  
 }
 function getClientValue() {
+  $("#getName").show()
   var title = document.getElementById("client_name").value;
-  document.getElementById("getName").innerText = title + ",";
+  document.getElementById("getName").innerText ="Hi" + " " + title + ",";
 }
 
 function getProjectName() {
-  var title = document.getElementById("project_name").value;
-  document.getElementById("cl_name").innerText = title;
-  document.getElementById("cl2_name").innerText = title;
-}
-
-function changeTlName() {
-  var title = document.getElementById("tl_name").value;
-  document.getElementById("tl_name_info").innerText = title;
-}
-
-$(document).ready(function () {
+  $("#cl_name").show()
+  $("#hr").show()
   var dateObj = new Date();
   var month = dateObj.getUTCMonth() + 1;
   var day = dateObj.getUTCDate();
@@ -44,9 +36,30 @@ $(document).ready(function () {
   newdate = `${day}th ${finalMonth}, ${year}`;
   $("#date").text(newdate);
   $("#date2").text(newdate);
+  var title = document.getElementById("project_name").value;
+  document.getElementById("cl_name").innerText = "Updates for "+ title + " As on " + day + "th" + " " + finalMonth + "," + " " + year ;
+  document.getElementById("cl2_name").innerText = "Following Are The Updates for " + title + " As On "+ day + "th" + " " + finalMonth + "," + " " + year;
+}
 
+function changeTlName() {
+  var title = document.getElementById("tl_name").value;
+  document.getElementById("tl_name_info").innerText =  "Thanks " + title;
+}
+
+$(document).ready(function () {
+  $("#getName").hide()
+  $("#cl_name").hide()
+  $("#hr").hide()
+  $("#comtask").hide()
+  $("#protask").hide()
+  $("#remaintask").hide()
+  $("#Queries").hide()
+  $("#notestitle").hide()
+  $("#info").hide()
   $("#completed_task").change(function () {
+    $("#comtask").show()
     $("#error_task").hide();
+    $("#info").show()
     var value = $("#completed_task").val();
 
     $("#items").append("<li> " + value + "[Done] " + "  </li>");
@@ -56,6 +69,8 @@ $(document).ready(function () {
 
   $("#progress_task").change(function () {
     $("#progress_task_error").hide();
+    $("#protask").show()
+    $("#info").show()
     var value = $("#progress_task").val();
 
     $("#progress_items").append("<li> " + value + "[Done] " + " </li>");
@@ -63,6 +78,8 @@ $(document).ready(function () {
 
   $("#remaining_task").change(function () {
     $("#remaining_task_error").hide();
+    $("#remaintask").show()
+    $("#info").show()
     var value = $("#remaining_task").val();
 
     $("#add_remaining_task").append("<li> " + value + "[Done] " + " </li>");
@@ -70,12 +87,16 @@ $(document).ready(function () {
 
   $("#queries").change(function () {
     $("#query_error").hide();
+    $("#Queries").show()
+    $("#info").show()
     var value = $("#queries").val();
     $("#add_queries").append("<li> " + value + "[Done] " + " </li>");
   });
 
   $("#notes").change(function () {
     $("#notes_error").hide();
+    $("#notestitle").show()
+    $("#info").show()
     var value = $("#notes").val();
     $("#add_notes").append("<li> " + value + "[Done] " + " </li>");
   });
@@ -287,113 +308,3 @@ function deleteNoteInput(counter) {
   $("#item" + counter).remove();
 }
 
-// function Based Program
-
-// let finalvalues = [];
-// function addValue(id) {
-//   if (id === "client_name") {
-//     var value = document.getElementById(id).value;
-//     document.getElementById("getName").innerText = value;
-//   }
-//   if (id === "project_name") {
-//     var value = document.getElementById(id).value;
-//     document.getElementById("cl_name").innerText = value;
-//     document.getElementById("cl2_name").innerText = value;
-//   }
-  
-// }
-// // console.log(finalvalues);
-// $(document).ready(function () {
-  
-//     var dateObj = new Date();
-//     var month = dateObj.getUTCMonth() + 1;
-//     var day = dateObj.getUTCDate();
-//     var year = dateObj.getUTCFullYear();
-  
-//     var mL = [
-//       "January",
-//       "February",
-//       "March",
-//       "April",
-//       "May",
-//       "June",
-//       "July",
-//       "August",
-//       "September",
-//       "October",
-//       "November",
-//       "December",
-//     ];
-//     const finalMonth = mL[month - 1];
-//     // newdate = year + "/" + month + "/" + day;
-//     newdate = `${day}th ${finalMonth}, ${year}`;
-//     $("#date").text(newdate);
-//     $("#date2").text(newdate);
-
-//     // Get Values from Each Field
-//     $("#completed_task").change(function () {
-//           $("#error_task").hide();
-//           var value = $("#completed_task").val();
-      
-//           $("#items").append("<li> " + value + "[Done] " + "  </li>");
-//         });
-    
-
-//   })
-
-
-  // All in One Function
-
-
-  // function mainButton(id,notes,notes_error,add_notes)
-  // {
-  //   var getFieldValue = $("#notes").val()
-
-  //   if(getFieldValue === "")
-  //   {
-  //     $("notes_error").text("Please Fill Exising Field")
-  //   }
-  //   else {
-  //             $("#notes_error").hide();
-        
-  //             counter += 1;
-  //             $("#add_notes").append(
-  //               '<div class="form-group" id="queries_div' +
-  //                 counter +
-  //                 '"> <input class="form-control" id="quries_input' +
-  //                 counter +
-  //                 '" onchange="AddValueToQueryTask( ' +
-  //                 counter +
-  //                 ' )"/> <button class="btn btn-danger" onclick="deleteQueryInput(' +
-  //                 counter +
-  //                 ')"> - </button> </div>'
-  //             );
-  //           }
-    
-
-  // }
-  // $("#query_task_btn").click(function (e) {
-  //       e.preventDefault();
-    
-  //       var getFieldValue = $("#queries").val();
-    
-  //       if (getFieldValue === "") {
-  //         $("#query_error").text("Please Fill Exising Field..");
-  //       } else {
-  //         $("#query_error").hide();
-    
-  //         counter += 1;
-  //         $("#query_task").append(
-  //           '<div class="form-group" id="queries_div' +
-  //             counter +
-  //             '"> <input class="form-control" id="quries_input' +
-  //             counter +
-  //             '" onchange="AddValueToQueryTask( ' +
-  //             counter +
-  //             ' )"/> <button class="btn btn-danger" onclick="deleteQueryInput(' +
-  //             counter +
-  //             ')"> - </button> </div>'
-  //         );
-  //       }
-  //     });
- 
