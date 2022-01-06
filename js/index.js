63a872b42aa798b5ae72772e773b4a2ebe7375f4
Vijay Koladara <abc.vijaykoladara212@gmail.com>
2022-01-06 12:30:09 +0530

    Add Object Functionality to Reduce Code

--- js/index.js
+++ js/index.js
@@ -64,258 +64,157 @@ function changeTlName() {
   document.getElementById("tl_name_info").innerHTML = "Thanks, <br/>" + title;
 }
 
-$(document).ready(function () {
-  $("#getName").hide();
-  $("#cl_name").hide();
-  $("#hr").hide();
-  $("#comtask").hide();
-  $("#protask").hide();
-  $("#remaintask").hide();
-  $("#Queries").hide();
-  $("#notestitle").hide();
-  $("#info").hide();
-  $("#completed_task").change(function () {
-    $("#comtask").show();
-    $("#error_task").hide();
-    $("#info").show();
-    var value = $("#completed_task").val();
+$("#getName").hide();
+$("#cl_name").hide();
+$("#hr").hide();
+$("#comtask").hide();
+$("#protask").hide();
+$("#remaintask").hide();
+$("#Queries").hide();
+$("#notestitle").hide();
+$("#info").hide();
+$("#completed").change(function () {
+  $("#comtask").show();
+  $("#completed_error").hide();
+  $("#info").show();
+  var value = $("#completed").val();
 
-    $("#items").append("<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + "  </li>");
-  });
-
-  // In Progress Task
-
-  $("#progress_task").change(function () {
-    $("#progress_task_error").hide();
-    $("#protask").show();
-    $("#info").show();
-    var value = $("#progress_task").val();
-
-    $("#progress_items").append("<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>");
-  });
-
-  $("#remaining_task").change(function () {
-    $("#remaining_task_error").hide();
-    $("#remaintask").show();
-    $("#info").show();
-    var value = $("#remaining_task").val();
-
-    $("#add_remaining_task").append("<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>");
-  });
-
-  $("#queries").change(function () {
-    $("#query_error").hide();
-    $("#Queries").show();
-    $("#info").show();
-    var value = $("#queries").val();
-    $("#add_queries").append("<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>");
-  });
-
-  $("#notes").change(function () {
-    $("#notes_error").hide();
-    $("#notestitle").show();
-    $("#info").show();
-    var value = $("#notes").val();
-    $("#add_notes").append("<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done]</span> " + " </li>");
-  });
-
-  // Add More Field for Completed Task
-
-  $("#task_btn").click(function (e) {
-    e.preventDefault();
-    var getData = $("#completed_task").val();
-
-    if (getData === "") {
-      $("#error_task").text("Please Fill Exising Field..");
-    } else {
-      $("#error_task").hide();
-      // $("#items").append("<li> " + getData + "  </li>");
-      counter += 1;
-      $("#add_more_task").append(
-        '<div class="form-group" id="box' +
-          counter +
-          '"> <input class="form-control" placeholder="Done Task" id="input' +
-          counter +
-          '" onchange="AddValue(' +
-          counter +
-          ')"/> <button class="btn btn-danger" onclick="remove_box(' +
-          counter +
-          ')">-</button> </div>'
-      );
-    }
-  });
-
-  // Add More Field for in - Progress Task
-
-  $("#progress_task_btn").click(function (e) {
-    e.preventDefault();
-
-    var getFieldValue = $("#progress_task").val();
-
-    if (getFieldValue === "") {
-      $("#progress_task_error").text("Please Fill Exising Field..");
-    } else {
-      $("#progress_task_error").hide();
-
-      counter += 1;
-      $("#add_more_progress_task").append(
-        '<div class="form-group" id="pro_div' +
-          counter +
-          '"> <input class="form-control" placeholder="Done Task" id="pro_input' +
-          counter +
-          '" onchange="AddValueToProgTask( ' +
-          counter +
-          ' )"/> <button class="btn btn-danger" onclick="deleteProInput(' +
-          counter +
-          ')"> - </button> </div>'
-      );
-    }
-  });
-
-  $("#remaining_task_btn").click(function (e) {
-    e.preventDefault();
-
-    var getFieldValue = $("#remaining_task").val();
-
-    if (getFieldValue === "") {
-      $("#remaining_task_error").text("Please Fill Exising Field..");
-    } else {
-      $("#remaining_task_error").hide();
-
-      counter += 1;
-      $("#add_more_remaining_task").append(
-        '<div class="form-group" id="remaining_div' +
-          counter +
-          '"> <input class="form-control" placeholder="Done Task" id="remaining_input' +
-          counter +
-          '" onchange="AddValueToRemainingTask( ' +
-          counter +
-          ' )"/> <button class="btn btn-danger" onclick="deleteRemainingInput(' +
-          counter +
-          ')"> - </button> </div>'
-      );
-    }
-  });
-
-  // Queries
-
-  $("#query_task_btn").click(function (e) {
-    e.preventDefault();
-
-    var getFieldValue = $("#queries").val();
-
-    if (getFieldValue === "") {
-      $("#query_error").text("Please Fill Exising Field..");
-    } else {
-      $("#query_error").hide();
-
-      counter += 1;
-      $("#query_task").append(
-        '<div class="form-group" id="queries_div' +
-          counter +
-          '"> <input class="form-control" placeholder="Done Task" id="quries_input' +
-          counter +
-          '" onchange="AddValueToQueryTask( ' +
-          counter +
-          ' )"/> <button class="btn btn-danger" onclick="deleteQueryInput(' +
-          counter +
-          ')"> - </button> </div>'
-      );
-    }
-  });
-
-  $("#notes_task_btn").click(function (e) {
-    e.preventDefault();
-
-    var getFieldValue = $("#notes").val();
-
-    if (getFieldValue === "") {
-      $("#notes_error").text("Please Fill Exising Field..");
-    } else {
-      $("#notes_error").hide();
-
-      counter += 1;
-      $("#note_tasks").append(
-        '<div class="form-group" id="notes_div' +
-          counter +
-          '"> <input class="form-control" placeholder="Done Task" id="notes_input' +
-          counter +
-          '" onchange="AddValueToNotesTask( ' +
-          counter +
-          ' )"/> <button class="btn btn-danger" onclick="deleteNoteInput(' +
-          counter +
-          ')"> - </button> </div>'
-      );
-    }
-  });
+  $("#add_completed_task").append(
+    "<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + "  </li>"
+  );
 });
 
-// Add Value to the List
-function AddValue(id) {
-  let value = $("#input" + id).val();
-  $("#items").append(
-    "<li id='Valueitem" + id + "'> " + value + " " + "<span style='font-weight:bold'>&nbsp;[Done] " + "  </li>"
+// In Progress Task
+
+$("#progress").change(function () {
+  $("#progress_error").hide();
+  $("#protask").show();
+  $("#info").show();
+  var value = $("#progress").val();
+
+  $("#add_progress_task").append(
+    "<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
   );
-}
+});
 
-// Remove Item Box
-function remove_box(counter) {
-  $("#box" + counter).remove();
-  $("#Valueitem" + counter).remove();
-}
+$("#remaining").change(function () {
+  $("#remaining_task_error").hide();
+  $("#remaintask").show();
+  $("#info").show();
+  var value = $("#remaining").val();
 
-// List of in Progress Task
-
-function AddValueToProgTask(id) {
-  let value = $("#pro_input" + id).val();
-  $("#progress_items").append(
-    "<li id='Progressitem" + id + "'> " + value + " " + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
-  );
-}
-
-// Remove Progress Task Extra Field
-
-function deleteProInput(counter) {
-  $("#pro_div" + counter).hide();
-  $("#Progressitem" + counter).remove();
-}
-
-// List of Remaning Task
-
-function AddValueToRemainingTask(id) {
-  let value = $("#remaining_input" + id).val();
   $("#add_remaining_task").append(
-    "<li id='Remainingitem" + id + "'> " + value + " " + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
+    "<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
   );
-}
+});
 
-// Remove Progress Task Extra Field
-
-function deleteRemainingInput(counter) {
-  $("#remaining_div" + counter).hide();
-  $("#Remainingitem" + counter).remove();
-}
-
-// Query
-
-function AddValueToQueryTask(id) {
-  let value = $("#quries_input" + id).val();
+$("#query").change(function () {
+  $("#query_error").hide();
+  $("#Queries").show();
+  $("#info").show();
+  var value = $("#query").val();
   $("#add_queries").append(
-    "<li id='Queryitem" + id + "'> " + value + " " + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
+    "<li> " + value + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
   );
+});
+
+$("#notes").change(function () {
+  $("#notes_error").hide();
+  $("#notestitle").show();
+  $("#info").show();
+  var value = $("#notes").val();
+  $("#add_notes").append(
+    "<li> " +
+      value +
+      "<span style='font-weight:bold'>&nbsp;[Done]</span> " +
+      " </li>"
+  );
+});
+
+const test = [
+  {
+    clickAbleElementId : "completed_task_btn",
+    parentInputElementId: "completed",
+    notesTask : "CompletedTask",
+    noteinput : "CompletedInput"
+  },
+  {
+    clickAbleElementId : "progress_task_btn",
+    parentInputElementId: "progress",
+    notesTask : "ProgressTask",
+    noteinput : "ProgressInput"
+  },
+  {
+    clickAbleElementId : "remaining_task_btn",
+    parentInputElementId: "remaining",
+    notesTask : "RemainingTask",
+    noteinput : "RemainingInput"
+  },
+  {
+    clickAbleElementId: "query_task_btn",
+    parentInputElementId: "query",
+    notesTask: "QueryTask",
+    noteinput: "QueryInput",
+  },
+  {
+    clickAbleElementId: "notes_task_btn",
+    parentInputElementId: "notes",
+    notesTask: "NotesTask",
+    noteinput: "NoteInput",
+  }
+];
+
+for (let index = 0; index < test.length; index++) {
+  const element = test[index];
+  $(`#${element.clickAbleElementId}`).click(function (e) {
+    console.log("Button clicked");
+    e.preventDefault();
+
+    var getFieldValue = $(`#${element.parentInputElementId}`).val();
+
+    if (getFieldValue === "") {
+      $(`#${element.parentInputElementId}_error`).text(
+        "Please Fill Exising Field.."
+      );
+    } else {
+      $(`#${element.parentInputElementId}_error`).hide();
+
+      counter += 1;
+      $(`#${element.parentInputElementId}_tasks`).append(
+        '<div class="form-group" id="' +
+          `${element.parentInputElementId}_div` +
+          counter +
+          '"> <input class="form-control" placeholder="Done Task" id="' +
+          `${element.parentInputElementId}_input` +
+          counter +
+          '" onchange="' +
+          `AddValueTo${element.notesTask}` +
+          "( " +
+          counter +
+          ' )"/> <button class="btn btn-danger" onclick="' +
+          `delete${element.noteinput}` +
+          "(" +
+          counter +
+          ')"> - </button> </div>'
+      );
+    }
+
+    
+  });
 }
 
-// Remove Progress Task Extra Field
 
-function deleteQueryInput(counter) {
-  $("#queries_div" + counter).hide();
-  $("#Queryitem" + counter).remove();
-}
 
 function AddValueToNotesTask(id) {
   let value = $("#notes_input" + id).val();
   $("#add_notes").append(
-    "<li id='item" + id + "'> " + value + " " + "<span style='font-weight:bold'>&nbsp;[Done] " + " </li>"
+    "<li id='item" +
+      id +
+      "'> " +
+      value +
+      " " +
+      "<span style='font-weight:bold'>&nbsp;[Done] " +
+      " </li>"
   );
 }
 
@@ -325,3 +224,84 @@ function deleteNoteInput(counter) {
   $("#notes_div" + counter).hide();
   $("#item" + counter).remove();
 }
+
+function AddValueToQueryTask(id) {
+  let value = $("#query_input" + id).val();
+  $("#add_queries").append(
+    "<li id='item" +
+      id +
+      "'> " +
+      value +
+      " " +
+      "<span style='font-weight:bold'>&nbsp;[Done] " +
+      " </li>"
+  );
+}
+
+// Remove Progress Task Extra Field
+
+function deleteQueryInput(counter) {
+  $("#query_div" + counter).hide();
+  $("#item" + counter).remove();
+}
+
+function AddValueToRemainingTask(id) {
+  let value = $("#remaining_input" + id).val();
+  $("#add_remaining_task").append(
+    "<li id='item" +
+      id +
+      "'> " +
+      value +
+      " " +
+      "<span style='font-weight:bold'>&nbsp;[Done] " +
+      " </li>"
+  );
+}
+
+// Remove Progress Task Extra Field
+
+function deleteRemainingInput(counter) {
+  $("#remaining_div" + counter).hide();
+  $("#item" + counter).remove();
+}
+
+function AddValueToProgressTask(id) {
+  let value = $("#progress_input" + id).val();
+  $("#add_progress_task").append(
+    "<li id='item" +
+      id +
+      "'> " +
+      value +
+      " " +
+      "<span style='font-weight:bold'>&nbsp;[Done] " +
+      " </li>"
+  );
+}
+
+// Remove Progress Task Extra Field
+
+function deleteProgressInput(counter) {
+  $("#progress_div" + counter).hide();
+  $("#item" + counter).remove();
+}
+
+function AddValueToCompletedTask(id) {
+  let value = $("#completed_input" + id).val();
+  $("#add_completed_task").append(
+    "<li id='item" +
+      id +
+      "'> " +
+      value +
+      " " +
+      "<span style='font-weight:bold'>&nbsp;[Done] " +
+      " </li>"
+  );
+}
+
+// Remove Progress Task Extra Field
+
+function deleteCompletedInput(counter) {
+  $("#completed_div" + counter).hide();
+  $("#item" + counter).remove();
+}
+
